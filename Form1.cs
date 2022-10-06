31fdf510c6979a3998e844fea54b27d9b232f1a2
Tarlan Muradli <terlanmuradli075@mail.ru>
2022-10-06 09:22:22 +0400

    Added some functionality

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,12 @@
+using System.Net.Http.Headers;
+
 namespace perimeter
 {
     public partial class pyramidCalculator : Form
     {
+
+        const double pi = 3.14;
+
         public pyramidCalculator()
         {
             InitializeComponent();
@@ -52,6 +57,85 @@ namespace perimeter
             txtHeight.Text = "";
             txtLength.Text = "";
             txtWidth.Text = "";
+        }
+
+        private void btnCalcCone_Click(object sender, EventArgs e)
+        {
+
+            if (txtHghtCone.Text == "" || txtRadCone.Text == "")
+            {
+                MessageBox.Show("Maku sure you entered each parameter!", "Unassigned Value", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else
+            {
+
+                double h = Convert.ToDouble(txtHghtCone.Text);
+                double r = Convert.ToDouble(txtRadCone.Text);
+
+                if (h <= 0 || r <= 0)
+                {
+                    MessageBox.Show("Invalid value - Please enter valid value!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                else
+                {
+                    // VOLUME
+                    double vol = pi * Math.Pow(r, 2) * h / 3;
+                    txtVolCone.Text = vol.ToString();
+
+                    // SURFACE AREA
+                    double area = pi * r * (Math.Sqrt(Math.Pow(h, 2) + Math.Pow(r, 2)) + r);
+                    txtAreaCone.Text = area.ToString();
+                }
+            }
+        }
+
+        private void btnClearCone_Click(object sender, EventArgs e)
+        {
+            txtAreaCone.Text = "";
+            txtVolCone.Text = "";
+            txtHghtCone.Text = "";
+            txtRadCone.Text = "";
+        }
+
+        private void btnCalcCyl_Click(object sender, EventArgs e)
+        {
+            if (txtHghtCyl.Text == "" || txtRadCyl.Text == "")
+            {
+                MessageBox.Show("Maku sure you entered each parameter!", "Unassigned Value", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else
+            {
+
+                double h = Convert.ToDouble(txtHghtCyl.Text);
+                double r = Convert.ToDouble(txtRadCyl.Text);
+
+                if (h <= 0 || r <= 0)
+                {
+                    MessageBox.Show("Invalid value - Please enter valid value!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                else
+                {
+                    // VOLUME
+                    double vol = pi * Math.Pow(r, 2) * h;
+                    txtVolCyl.Text = vol.ToString();
+
+                    // SURFACE AREA
+                    double area = 2 * pi * r * (h + r);
+                    txtAreaCyl.Text = area.ToString();
+                }
+            }
+        }
+
+        private void btnClearCyl_Click(object sender, EventArgs e)
+        {
+            txtAreaCyl.Text = "";
+            txtVolCyl.Text = "";
+            txtHghtCyl.Text = "";
+            txtRadCyl.Text = "";
         }
     }
 }
